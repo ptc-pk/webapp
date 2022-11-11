3ef27642e7fc07a608566c509b46e0ba51ebcdcb
Phuchon Chueapong <114440261+phuchon-chueapong@users.noreply.github.com>
2022-11-11 20:50:48 +0700

    Setup Footer static component

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -3,8 +3,12 @@ import './Footer.css';
 
 function FooterStaticComponent() {
   return (
-    <footer>
-      <p>footer is works!</p>
+    <footer className='ptc-static ptc-ft-container'>
+      <div className='ptc-content ptc-ft-content'>
+        <div className='ptc-ft-card'>Left</div>
+        <div className='ptc-ft-card'>Center</div>
+        <div className='ptc-ft-card'>Right</div>
+      </div>
     </footer>
   )
 }
